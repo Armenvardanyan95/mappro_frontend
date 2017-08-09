aa01a5333f00d3a4108006e2235c8e789d04972d
Armenvardanyan95 <armenvardanyan95@gmail.com>
2017-08-09 21:26:43 +0400

    Push

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import {MdDialog} from '@angular/material';
+
+import {ColorMarkerComponent} from  'app/color-marker/color-marker.component';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  @Input() colors: IColorMarker[];
+  showNewOrderForm: boolean = false;
+  @Output()
+  showNewOrderFormChange: EventEmitter<boolean> = new EventEmitter();
+  constructor(private dialog: MdDialog) { }
 
   ngOnInit() {
+  }
+
+  toggleShowNewOrderForm(){
+    this.showNewOrderForm = !this.showNewOrderForm;
+    this.showNewOrderFormChange.emit(this.showNewOrderForm);
+  }
+
+  openNewColorDialog(): void {
+    const dialog = this.dialog.open(ColorMarkerComponent);
   }
 
 }
